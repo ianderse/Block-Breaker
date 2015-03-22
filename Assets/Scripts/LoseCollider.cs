27217ef03425260529add1758286539e401a63cf
Ian Andersen <ianderse@mac.com>
2015-03-22 10:56:57 -0600

    initial commit; Unity Course Project

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		levelManager.LoadLevel("Win Screen");
+	}
+	
+	void OnCollisonEnter2D(Collision2D collision) {
+	
+	}
+}
